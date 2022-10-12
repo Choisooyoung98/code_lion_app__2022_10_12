a487c34bc79edf916b292fb81c2baefdf6e265fb
Choisooyoung98 <ekfms15815@gmail.com>
2022-10-12 15:03:40 +0900

    세팅

---
+++ src/test/java/com/ll/exam/app__2022_10_12/App20221012ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app__2022_10_12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App20221012ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
