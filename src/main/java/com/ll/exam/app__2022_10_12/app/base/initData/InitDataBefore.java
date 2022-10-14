dd71ce7dad0706e624d38fbed24b46926ef0a05a
Choisooyoung98 <ekfms15815@gmail.com>
2022-10-14 10:19:21 +0900

    장바구니 서비스 구현

--- src/main/java/com/ll/exam/app__2022_10_12/app/base/initData/InitDataBefore.java
+++ src/main/java/com/ll/exam/app__2022_10_12/app/base/initData/InitDataBefore.java
@@ -1,13 +1,16 @@
 package com.ll.exam.app__2022_10_12.app.base.initData;
 
+import com.ll.exam.app__2022_10_12.app.cart.entity.CartItem;
+import com.ll.exam.app__2022_10_12.app.cart.service.CartService;
 import com.ll.exam.app__2022_10_12.app.member.entity.Member;
 import com.ll.exam.app__2022_10_12.app.member.service.MemberService;
+import com.ll.exam.app__2022_10_12.app.product.entity.Product;
 import com.ll.exam.app__2022_10_12.app.product.service.ProductService;
 import com.ll.exam.app__2022_10_12.app.song.entity.Song;
 import com.ll.exam.app__2022_10_12.app.song.service.SongService;
 
 public interface InitDataBefore {
-    default void before(MemberService memberService, SongService songService, ProductService productService) {
+    default void before(MemberService memberService, SongService songService, ProductService productService, CartService cartService) {
         Member member1 = memberService.join("user1", "1234", "user1@test.com");
         Member member2 = memberService.join("user2", "1234", "user2@test.com");
 
@@ -20,11 +23,16 @@ public interface InitDataBefore {
         Song song7 = songService.create(member2, "노래 7", "내용 7");
         Song song8 = songService.create(member2, "노래 8", "내용 8");
 
-        productService.create(song1, "그리움", 1_900);
-        productService.create(song3, "사건의 지평선", 3_900);
-        productService.create(song5, "허리춤", 2_900);
-        productService.create(song7, "Monologue", 4_900);
-        productService.create(song8, "Braindead", 6_900);
+        Product product1 = productService.create(song1, "그리움", 1_900);
+        Product product2 = productService.create(song3, "사건의 지평선", 3_900);
+        Product product3 = productService.create(song5, "허리춤", 2_900);
+        Product product4 = productService.create(song7, "Monologue", 4_900);
+        Product product5 = productService.create(song8, "Braindead", 6_900);
+
+        CartItem cartItem1 = cartService.addItem(member1, product1);
+        CartItem cartItem2 = cartService.addItem(member1, product2);
+        CartItem cartItem3 = cartService.addItem(member2, product3);
+        CartItem cartItem4 = cartService.addItem(member2, product4);
 
     }
 }
--- src/main/java/com/ll/exam/app__2022_10_12/app/product/controller/ProductController.java
+++
@@ -1,7 +0,0 @@
-package com.ll.exam.app__2022_10_12.app.product.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class ProductController {
-}
